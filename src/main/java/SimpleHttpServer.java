76ef73a908ab8e527ea27be67fbc92c680fabfc5
shishirkarki <skarki2@gmail.com>
2025-05-24 22:05:23 +0545

    feat: parse HTTP headers and support query parameters - Read and store all HTTP request headers in a map - Parse query parameters from the URL - Support dynamic responses based on query params (e.g., /hello?name=) - Support response based on headers (e.g., User-Agent on /agent route) - Enhanced routing logic to handle URL paths without query strings

--- src/main/java/SimpleHttpServer.java
+++ src/main/java/SimpleHttpServer.java
@@ -2,7 +2,10 @@ package main.java;
 
 import java.io.*;
 import java.net.*;
+import java.nio.Buffer;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleHttpServer{
     public static void main(String[] args) throws IOException{
@@ -36,26 +39,35 @@ public class SimpleHttpServer{
             String[] parts = requestLine.split(" ");
             if (parts.length < 2) return;
 
-            String method = parts[0];
-            String path = parts[1];
+            String method = parts[0]; // GET POST
+            String rawPath = parts[1]; // API path + queryParams
+            String pathOnly = rawPath.split("\\?")[0]; // API path
+
+            Map<String, String> queryParams = parseQueryParams(rawPath);
+            Map<String, String> headers = parseHeaders(in);
 
             String responseBody;
-            switch (path) {
+            String status = "";
+            switch (pathOnly) {
                 case "/":
                     responseBody = "Welcome to my simple HTTP server!";
                     break;
                 case "/hello":
-                    responseBody = "Hello from the server!";
+                    String name = queryParams.getOrDefault("name", "stranger");
+                    responseBody = "Hello "+name+"!";
+                    status = "HTTP/1.1 200 OK";
                     break;
-                case "/time":
-                    responseBody = LocalDateTime.now().toString();
+                case "/agent":
+                    String userAgent = headers.getOrDefault("User-Agent", "unknown");
+                    responseBody = "Your user agent is: "+ userAgent;
+                    status = "HTTP/1.1 200 OK";
                     break;
                 default:
                     responseBody = "404 Not Found";
-                    break;
+                    status = "HTTP/1.1 404 Not Found";
             }
 
-            String httpResponse = "Http/1.1 " + (path.equals("/") || path.equals("/hello") || path.equals("/time") ? "200 OK" : "404 Not Found") + "\r\n" +
+            String httpResponse = "Http/1.1 " + status + "\r\n" +
                     "Content-Type: text/plain\r\n" +
                     "Content-Length:" + responseBody.length() + "\r\n" +
                     "\r\n" +
@@ -68,5 +80,41 @@ public class SimpleHttpServer{
         }finally {
                 clientSocket.close();
         }
+    }
+
+    private static Map<String, String> parseHeaders(BufferedReader in) throws IOException{
+            Map<String, String> headers = new HashMap<>();
+            String line;
+            while ((line = in.readLine()) != null && !line.isEmpty()) {
+                int idx = line.indexOf(":");
+                if(idx != -1){
+                    String headerName = line.substring(0, idx).trim();
+                    String headerValue = line.substring(idx + 1).trim();
+                    headers.put(headerName, headerValue);
+                }
+            }
+            return headers;
+    }
+
+    private static Map<String, String> parseQueryParams(String rawPath){
+        try {
+            Map<String, String> queryParams = new HashMap<>();
+            if (rawPath.contains("?")) {
+                String queryString = rawPath.split("\\?", 2)[1];
+                for (String param : queryString.split("&")) {
+                    String[] kv = param.split("=");
+                    if (kv.length == 2) {
+                        queryParams.put(URLDecoder.decode(kv[0], "UTF-8"),
+                                URLDecoder.decode(kv[1], "UTF-8"));
+                    }
+                }
+            }
+
+            return queryParams;
+        }catch (UnsupportedEncodingException ex){
+            ex.printStackTrace();
+        }
+
+        return new HashMap<>();
     }
 }
